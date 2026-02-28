fae2e2141f52410a7a7a9ac8db62ac7ca5275950
dorinelrushi <rushidorinel@gmail.com>
2026-02-28 23:08:21 +0100

    ls i

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Navbar from './components/Navbar';
@@ -20,6 +21,16 @@ export default function RootLayout({ children }) {
           crossOrigin="" />
       </head>
       <body suppressHydrationWarning={true} className={inter.className}>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-TMCLY9JCVR" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-TMCLY9JCVR');
+          `}
+        </Script>
         <Providers>
           <Navbar />
           <main>
